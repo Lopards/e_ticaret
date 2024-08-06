36ba769dc0685965310882cf61d7f23f3b6cb986
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2024-08-06 13:22:54 +0300

    sitede türkiye saati geçerli olacak

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'tr-tr'
+LANGUAGE_CODE = 'tr-tr' # sitenin türkçe olması için
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Istanbul' #türkiye saati
 
 USE_I18N = True
 

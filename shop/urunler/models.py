3c6e635a2e8c68ce4d6d377614360d3cc39a8a4f
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2024-08-07 17:49:30 +0300

    kategori kısmı eklendi

--- shop/urunler/models.py
+++ shop/urunler/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Kategoriler(models.Model):
+    name = models.CharField(max_length=50) #text alanı oluşturur
+    is_active = models.BooleanField(default=True)#kategorinin aktif mi değil mi
+
+    class Meta:
+        verbose_name_plural = "Kategoriler"
+        verbose_name = "Kategori"
+    def __str__(self): # kategoriyi sitede göstermek için
+        return self.name

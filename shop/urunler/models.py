d8d252842f7b99d380db5c8cd465a6cc937aae35
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2024-08-08 12:07:04 +0300

    Update models.py

--- shop/urunler/models.py
+++ shop/urunler/models.py
@@ -10,3 +10,19 @@ class Kategoriler(models.Model):
         verbose_name = "Kategori"
     def __str__(self): # kategoriyi sitede göstermek için
         return self.name
+
+class Markalar(models.Model):
+    name = models.CharField(max_length=155)
+    aciklama = models.TextField(blank=True,null=True,help_text="")
+    seo_title = models.CharField(max_length=155,blank=True,null=True)
+    seo_description = models.TextField(blank=True)
+    slug = models.SlugField(blank=True,null=True,unique=True)
+    is_active = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='markaresimleri')
+
+    class Meta:
+        verbose_name_plural = "Markalar"
+        verbose_name = "Marka"
+
+        def __str__(self):
+            return self.verbose_name_plural

cf19f8152121ab5b8fafa7d8ac4608767a976021
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2024-08-11 14:05:20 +0300

    urunler ve markalar düzenlendi
    many to many

--- shop/urunler/admin.py
+++ shop/urunler/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
-from.models import Kategoriler
-
+from .models import Kategoriler, Markalar, Urunler, Etiketler
+from django.utils.html import format_html
 # Register your models here.
+
+#admin.site.register(Kategoriler) #kategorileri admin paneline ekledim
+
 class KategorilerAdmin(admin.ModelAdmin):
     list_display =  ['name', 'seo_title', 'seo_description','slug', 'is_active']
     search_fields = ['name', 'seo_title', 'seo_description','slug']
     list_filter = ['is_active']
 
-admin.site.register(Kategoriler) #kategorileri admin paneline ekledim
+admin.site.register(Kategoriler, KategorilerAdmin)
 
-class MarkalarAdmin(admin.ModelAdmin): # markalar adlı bölümü admin paneline ekledim
+class MarkalarAdmin(admin.ModelAdmin):
     list_display = ['name', 'seo_title', 'seo_description', 'is_active', 'image_tag']
     search_fields = ['is_active', 'seo_title', 'name']
     list_filter = ['is_active', 'name']
@@ -22,3 +25,21 @@ class MarkalarAdmin(admin.ModelAdmin): # markalar adlı bölümü admin paneline
     image_tag.short_description = 'Resim'  #kullanılan resmin sitede gözükmesi için bu fonksiyon kullanıldı
 
 admin.site.register(Markalar, MarkalarAdmin)
+
+
+class UrunlerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'fiat', 'marka', 'indirimli_fiat', 'is_active', 'image_tag', 'tarih']
+    search_fields = ['name', 'kategori', 'is_active', 'marka']
+    list_filter = ['slug', 'seo_title', 'name']
+
+    def image_tag(self, obj):
+        if obj.resim:
+            return format_html('<img src="{}" style="width: 50px; height: 50px;" />'.format(obj.resim.url))
+        return "-"
+
+    image_tag.short_description = 'Resim'  # Admin panelinde bu başlık altında gözükecek
+
+
+admin.site.register(Urunler, UrunlerAdmin)
+
+admin.site.register(Etiketler)
